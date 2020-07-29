2dfc1dcdf7f51143b1d0a6fd55714359e7535c9a
eKoh951 <erick_huie2@hotmail.com>
2020-07-28 22:20:06 -0600

    Added broadcast location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,32 @@
-io()
+// socket.io-client is exposed automatically by the socket.io server as /socket.io/socket.io.js
+// we are importing that from the HTML file as: <script src="/socket.io/socket.io.js"></script>
+
+// Instance of socket-client
+// io() function is used to connect the Client to the Server
+// io() function returns a new Socket instance
+const client = io()
+
+client.on('message', (data) => {
+	console.log(data)
+})
+
+const msgForm = document.querySelector('form')
+
+msgForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	// e.target to get the form
+	const msgString = e.target.elements.message.value
+	client.emit('sendMessage', msgString)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if(!navigator.geolocation)
+		return alert('Geolocation is not suppoerted by your browser.')
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		client.emit('sendLocation', {
+			lat: position.coords.latitude,
+			long: position.coords.longitude
+		})
+	})
+})
--- src/index.js
+++ src/index.js
@@ -12,8 +12,26 @@ const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-io.on('connection', () => {
+const data = 'Welcome!'
+
+// io.on: Server method
+io.on('connection', (socket) => {
 	console.log('New WebSocket connection')
+
+	// Flag: 'broadcast' send to every client but to the current client
+	socket.broadcast.emit('message', 'A new user has joined!')
+
+	socket.on('sendMessage', (message) => {
+		io.emit('message', message)
+	})
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left!')
+	})
+
+	socket.on('sendLocation', (location) => {
+		io.emit('message', `https://google.com/maps?q=${location.lat},${location.long}`)
+	})
 })
 
 server.listen(port, () => console.log(`Listening to port ${3000}`))
